5845ba05b14c289c3f209bf484ff4266aef9d207
nishiko <nishiko@mindia.jp>
2022-03-26 04:50:54 +0000

    fix Sync.new.exec

--- app/models/sync.rb
+++ app/models/sync.rb
@@ -10,27 +10,28 @@ class Sync
     notion = Api::Notion.new
     client = notion.client
     notion.select_database(id).each do |db|
-      next if db['properties']['timecrowd_category_id']['rich_text'].present? && db['properties']['timecrowd_category_id']['rich_text'].first['text']['content'].present?
       title = db['properties']['Name']['title'].first['text']['content']
 
-      id = Api::Timecrowd.new.create_category(team_id, title, parent_id: parent_id, color: color)['id']
+      next if db['properties']['timecrowd_category_id']['number'].present?
 
-      category = Category.find_or_initialize_by(
-        timecrowd_category_id: id,
-      )
-      category.timecrowd_team_id = team_id
-      category.user_id = user_id
-      category.notion_database_id = db['id']
-      category.save!
+      puts title
+
+      category_id = Api::Timecrowd.new.create_category(team_id, title, parent_id: parent_id, color: color)['id']
 
 
-      # properties = {
-      #   'aaa': {
-      #     'text': 'ddd'
-      #   }
-      # }
-      # client.update_database(database_id: db['id'], properties: properties)
-      # client.update_page(page_id: db['id'], properties: properties)
+      # category = Category.find_or_initialize_by(
+      #   timecrowd_category_id: id,
+      # )
+      # category.timecrowd_team_id = team_id
+      # category.user_id = user_id
+      # category.notion_database_id = db['id']
+      # category.save!
+
+
+      properties = {
+        'timecrowd_category_id': category_id
+      }
+      client.update_page(page_id: db['id'], properties: properties)
     end
   end
 end
